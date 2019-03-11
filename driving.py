b5574517683bac170a42767239b36bcc4261e8f4
NJ Kuan <cannokuan@gmail.com>
2019-03-11 22:31:40 +0800

    add country

---
+++ driving.py
@@ -0,0 +1,13 @@
+country = input('What is your country?')
+age = input('What is your age?')
+age = int(age)
+if country == 'Taiwan':
+	if age >= 18:
+		print ('You can drive!!!')
+	else:
+		print ('You cannot drive!!!')
+elif country == 'America':
+	if age >= 16:
+		print ('You can drive!!!')
+	else:
+		print ('You cannot drive!!!')
